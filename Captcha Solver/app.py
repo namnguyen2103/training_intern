f766234c99a88936e82aa4575a149e119cdea279
Nam Nguyen <Nam.NH210630@sis.hust.edu.vn>
2024-10-17 16:08:08 +0700

    Refactor

--- Captcha Solver/app.py
+++ Captcha Solver/app.py
@@ -1,13 +1,14 @@
 from flask import Flask, request, jsonify
 from PIL import Image
 import torch
-from transformers import VisionEncoderDecoderModel, TrOCRProcessor
+from transformers import VisionEncoderDecoderConfig, VisionEncoderDecoderModel, TrOCRProcessor
 
 app = Flask(__name__)
-ckp_path = #Model weights are available at https://drive.google.com/file/d/1Ku2jl6ID9d20eNyBXTCbrRYF2Ekcz2IE
-model = VisionEncoderDecoderModel.from_pretrained("OCR-model/best")
+OCR_ckp_path = "best" #Model weights are available at https://drive.google.com/file/d/1Ku2jl6ID9d20eNyBXTCbrRYF2Ekcz2IE
+encoder_decoder_config = VisionEncoderDecoderConfig.from_pretrained(OCR_ckp_path)
+OCR_model = VisionEncoderDecoderModel.from_pretrained(OCR_ckp_path, config=encoder_decoder_config)
 processor = TrOCRProcessor.from_pretrained("microsoft/trocr-small-handwritten")
-model.eval()
+OCR_model.eval()
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -22,7 +23,7 @@ def predict():
     pixel_values = processor(image, return_tensors="pt").pixel_values
     
     with torch.no_grad():
-        generated_ids = model.generate(pixel_values)
+        generated_ids = OCR_model.generate(pixel_values)
     generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
     
     return jsonify({'captcha': generated_text})
